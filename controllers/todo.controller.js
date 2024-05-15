55cac979fa7a68e588e730a8eb7aa45702c8419f
mocktest <cjoekalda@gmail.com>
2024-05-15 14:00:44 +0300

    Create error handling for unit and integration tests, bugged next function

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,12 @@
 const TodoModel = require('../models/todo.model');
 
 const createTodo = async (req, res, next) => {
-    const createdModel = await TodoModel.create(req.body);
-    res.status(201).json(createdModel);
+    try {
+        const createdModel = await TodoModel.create(req.body);
+        res.status(201).json(createdModel);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
-module.exports = {createTodo}; 
+module.exports = { createTodo }; 
