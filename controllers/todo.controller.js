17f82d590f4c6b7346f582ef0efc0a6f2d3185f0
mocktest <cjoekalda@gmail.com>
2024-05-15 14:29:03 +0300

    Created TodoController.getTodos unit test and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -9,4 +9,13 @@ const createTodo = async (req, res, next) => {
     }
 };
 
-module.exports = { createTodo }; 
+const getTodos = async (req, res, next) => {
+    try {
+        const allTodos = await TodoModel.find({});
+        res.status(200).json(allTodos);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+};
+
+module.exports = { createTodo, getTodos }; 
