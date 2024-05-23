5d5808fe151a6b70d6c9ef1a20f7869faa407468
mocktest <cjoekalda@gmail.com>
2024-05-23 13:29:17 +0300

    Created TodoController.getTodoById unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,6 +4,8 @@ const newTodo = require('../mock-data/new-todo.json');
 
 const endpointUrl = '/todos/';
 
+let firstTodo;
+
 describe(endpointUrl, () => {
     it('POST ' + endpointUrl, async () => {
         const response = await request(app)
@@ -22,11 +24,26 @@ describe(endpointUrl, () => {
             message: "Todo validation failed: done: Path `done` is required."
         });
     });
-    test('GET ' + endpointUrl, async () => {
+    it('GET ' + endpointUrl, async () => {
         const response = await request(app).get(endpointUrl);
         expect(response.statusCode).toBe(200);
         expect(Array.isArray(response.body)).toBeTruthy();
         expect(response.body[0].title).toBeDefined();
         expect(response.body[0].done).toBeDefined();
+        firstTodo = response.body[0]; 
     });
+    it('GET by Id ' + endpointUrl + ':todoId', async () => {
+        const response = await request(app)
+        .get(endpointUrl + firstTodo._id);
+        expect(response.statusCode).toBe(200);
+        expect(response.body.title).toBe(firstTodo.title);
+        expect(response.body.done).toBe(firstTodo.done);
+    });
+    /*
+    it('GET todo by id doesnt exist' + endpointUrl + ':todoId', async () => {
+        const response = await request(app)
+        .get(endpointUrl + '664463bb7ba93efbd375e97b');
+        expect(response.statusCode).toBe(404);
+    });
+    */
 });
