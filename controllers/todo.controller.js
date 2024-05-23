46ca97fcb3c220846eb6c12c085f9a89a664b060
mocktest <cjoekalda@gmail.com>
2024-05-23 14:54:21 +0300

    Created TodoController.deleteTodo unit test and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -51,4 +51,17 @@ const updateTodo = async (req, res, next) => {
     }
 };
 
-module.exports = { createTodo, getTodos, getTodoById, updateTodo }; 
+const deleteTodo = async (req, res, next) => {
+    try {
+        const todoModel = await TodoModel.findByIdAndDelete(req.params.todoId);
+        if (todoModel) {
+            res.status(200).json(todoModel)
+        } else {
+            res.status(404).send()
+        }
+    } catch (error) {
+        next(error)
+    }
+};
+
+module.exports = { createTodo, getTodos, getTodoById, updateTodo, deleteTodo }; 
